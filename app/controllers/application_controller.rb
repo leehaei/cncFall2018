4b5430daf191479ebc17928995d87254d98cef41
leehaei <h283lee@edu.uwaterloo.ca>
2018-10-17 03:41:16 -0400

    Add static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  
+  def hello # added our own function to say something friendly
+    render html: "hello, world!"
+  end
 end
